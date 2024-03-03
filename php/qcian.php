1b24550bedc3be525921314f45709e2ab12f5171
Nathaniel Carlo M. Morva <morva.nathanielcarlo.macapagal@gmail.com>
2024-03-03 20:51:00 +0800

    draft qcian webpage done

--- php/qcian.php
+++ php/qcian.php
@@ -18,97 +18,160 @@
     <?php include('../html/header.html') ?>
     <div class="container">
         <h1 class="py-3">Hello, future QCian!</h1>
-        <h2 class="text-white" id="subHeading">GENERAL ADMISSION POLICY</h2>
-        <p id="qcian-details" class="p-5">A student desiring to enroll in Quezon City University has to comply with the following
-            Admission
-            requirements: <br> <br>
+        <div class="border border-black rounded-top-3 rounded-bottom-5" id="section">
+            <h2 class="text-white rounded-top-3" id="subHeading">GENERAL ADMISSION POLICY</h2>
+            <p id="qcian-details" class="p-5 rounded-bottom-5">A student desiring to enroll in Quezon City University
+                has to comply with the following
+                Admission
+                requirements: <br> <br>
 
-            1. Must pass the QCU Admission Test <br>
-            2. Must have at least a general weighted average of 80% or above for those who will take BSIT, BSENT,
-            and BSIE. For ESA, BSECE, and BECED, see their respective admission policy. <br>
-            3. Of good moral character<br>
-            4. Physically and Mentally fit to pursue a college education<br>
-            5. Must be a senior high school graduate, high school graduate (old curriculum), or ALS passer<br>
-            eligible for college<br>
-            6. Must pass the department interview</p>
-        <h2 class="text-white" id="subHeading">GENERAL ADMISSION POLICY</h2>
-        <p id="qcian-details" class="p-5">A student desiring to enroll in Quezon City University has to comply with the following
-            Admission
-            requirements: <br> <br>
+                1. Must pass the QCU Admission Test <br>
+                2. Must have at least a general weighted average of 80% or above for those who will take BSIT, BSENT,
+                and BSIE. For ESA, BSECE, and BECED, see their respective admission policy. <br>
+                3. Of good moral character<br>
+                4. Physically and Mentally fit to pursue a college education<br>
+                5. Must be a senior high school graduate, high school graduate (old curriculum), or ALS passer<br>
+                eligible for college<br>
+                6. Must pass the department interview</p>
+        </div>
+        <div class="border border-black rounded-top-3 rounded-bottom-5" id="section">
+            <h2 class="text-white" id="subHeading">SELECTION FOR A DEGREE PROGRAM AND CAMPUS</h2>
+            <p id="qcian-details" class="p-5 rounded-bottom-5">Qualified applicants for a given campus are ranked
+                according to their ratings
+                and the available slots of their preferred program. <br><br>
 
-            1. Must pass the QCU Admission Test <br>
-            2. Must have at least a general weighted average of 80% or above for those who will take BSIT, BSENT,
-            and BSIE. For ESA, BSECE, and BECED, see their respective admission policy. <br>
-            3. Of good moral character<br>
-            4. Physically and Mentally fit to pursue a college education<br>
-            5. Must be a senior high school graduate, high school graduate (old curriculum), or ALS passer<br>
-            eligible for college<br>
-            6. Must pass the department interview</p>
-        <h2 class="text-white" id="subHeading">GENERAL ADMISSION POLICY</h2>
-        <p id="qcian-details" class="p-5">A student desiring to enroll in Quezon City University has to comply with the following
-            Admission
-            requirements: <br> <br>
+                Applicants who do not qualify for their first choice of Degree Program or Campus will be channeled to
+                their
+                second or third choice depending on the availability of slots.<br><br>
 
-            1. Must pass the QCU Admission Test <br>
-            2. Must have at least a general weighted average of 80% or above for those who will take BSIT, BSENT,
-            and BSIE. For ESA, BSECE, and BECED, see their respective admission policy. <br>
-            3. Of good moral character<br>
-            4. Physically and Mentally fit to pursue a college education<br>
-            5. Must be a senior high school graduate, high school graduate (old curriculum), or ALS passer<br>
-            eligible for college<br>
-            6. Must pass the department interview</p>
-        <h2 class="text-white" id="subHeading">GENERAL ADMISSION POLICY</h2>
-        <p id="qcian-details" class="p-5">A student desiring to enroll in Quezon City University has to comply with the following
-            Admission
-            requirements: <br> <br>
+                'Once admitted and enrolled at QCU, you are not allowed to enroll in any other degree program at another
+                university.</p>
+        </div>
+        <div class="border border-black rounded-top-3 rounded-bottom-5" id="section">
+            <h2 class="text-white" id="subHeading">FRESHMEN ADMISSION REQUIREMENTS</h2>
+            <p id="qcian-details" class="p-5">Choose if you are Graduating Senior High School Student,
+                a SHS Graduate, High School Graduate. ALS Passers applicants can now register without F138 / REPORT CARD
+                on the condition that they will submit their grades to complete their application for evaluation.</p>
+            <table class="table" id="qcian-details">
+                <thead>
+                    <tr>
+                        <th scope="col" class="text-center">BS ENTREP, BSIT, BSIE </th>
+                        <th scope="col" class="text-center">BSA, BSECE, BECED </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1. Fully accomplished QCU College Admission Application Form with 2x2 picture (white
+                            background with name tag) and am.x Signature of Student over Printed Name on the space
+                            provided at the back of the form.</td>
+                        <td>Otto</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Larry the Bird</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p id="qcian-details" class="p-5 rounded-bottom-5 fw-bold text-center">Note: Admissions of Transferees
+                depends on the
+                availability of slots</p>
+        </div>
+        <div class="border border-black rounded-top-3 rounded-bottom-5" id="section">
+            <h2 class="text-white" id="subHeading">QCUCAT PROCEDURE:</h2>
+            <div id="qcian-details" class="p-5 rounded-bottom-5">
+                <h3>Step 1</h3>
+                <p>Register using the newly created g-mail account and upload the requirements below in the QCU College
+                    Admission Test (QCUCAT) Application Link. Remember your password because this will be used for
+                    receiving
+                    notifications about the status of your application, invite for department interview, for Admission
+                    and
+                    Enrollment purposes. You will be using this email for your entire stay at QCU.</p>
+                <h3>Step 2</h3>
+                <p> The Admissions Office will evaluate the applicants according to the completeness of their submitted
+                    requirements. Applicants with complete requirements will be endorsed to the Guidance Office for the
+                    schedule of examination.</p>
+                <h3>Step 3</h3>
+                <p> The Guidance Office will notify the applicants one week ahead of their scheduled examination through
+                    their registered email address indicating their schedule of examination with primary instructions
+                    together with the Google Meet link. Please check your email's spam/junk folder also where our email
+                    might be redirected.</p>
+                <h3>Step 4</h3>
+                <p> QCU CAT Passers will be referred to the Dean of their 1st preferred course for an interview and the
+                    results will be sent to the admissions office. (Examinees are advised to check their registered
+                    email or
+                    your contact numbers for the schedule of the interview.)</p>
+                <h3>Step 5</h3>
+                <p> A list of qualified students for Admission will be posted on June 2024, at qcu.edu.ph. Students will
+                    be
+                    informed to submit all the original hard copies of the documentary requirements indicated at the
+                    back of
+                    the QCU Admission Application Form to secure their enrollment slot. Submission will be via courier,
+                    Drop
+                    Boxes at the gate, or they will be given an appointment to submit their requirements. <br><br>
 
-            1. Must pass the QCU Admission Test <br>
-            2. Must have at least a general weighted average of 80% or above for those who will take BSIT, BSENT,
-            and BSIE. For ESA, BSECE, and BECED, see their respective admission policy. <br>
-            3. Of good moral character<br>
-            4. Physically and Mentally fit to pursue a college education<br>
-            5. Must be a senior high school graduate, high school graduate (old curriculum), or ALS passer<br>
-            eligible for college<br>
-            6. Must pass the department interview</p>
-        <h2 class="text-white" id="subHeading">GENERAL ADMISSION POLICY</h2>
-        <p id="qcian-details" class="p-5">A student desiring to enroll in Quezon City University has to comply with the following
-            Admission
-            requirements: <br> <br>
+                    Requirements should be sealed in a long brown envelope with a list of documents to be
+                    submitted.<br><br>
 
-            1. Must pass the QCU Admission Test <br>
-            2. Must have at least a general weighted average of 80% or above for those who will take BSIT, BSENT,
-            and BSIE. For ESA, BSECE, and BECED, see their respective admission policy. <br>
-            3. Of good moral character<br>
-            4. Physically and Mentally fit to pursue a college education<br>
-            5. Must be a senior high school graduate, high school graduate (old curriculum), or ALS passer<br>
-            eligible for college<br>
-            6. Must pass the department interview</p>
-        <h2 class="text-white" id="subHeading">GENERAL ADMISSION POLICY</h2>
-        <p id="qcian-details" class="p-5">A student desiring to enroll in Quezon City University has to comply with the following
-            Admission
-            requirements: <br> <br>
+                    Passing the entrance examination does not guarantee enrollment at QCU, passers must submit complete
+                    admission requirements to secure a slot for enrollment.</p>
+            </div>
+        </div>
+        <div class="border border-black rounded-top-3 rounded-bottom-5" id="section">
+            <h2 class="text-white" id="subHeading">ADMISSIONS GUIDELINES: Classifications</h2>
+            <div id="qcian-details" class="p-5 rounded-bottom-5">
+                <h3>1. FRESHMEN</h3>
+                <p>
+                    a. High School Graduates (2015 and below) or Senior High School Graduates who must not have enrolled
+                    in
+                    any academic or college subject/s prior to their enrollment as new college freshmen. <br>
+                    b. Students enrolled in 6 months and below vocational courses.<br>
+                    c. Alternative Learning System Passers (ALS)<br><br>
+                </p>
+                <h3>2. TRANSFEREES (admission of transferees are subject to the availability of slots)</h3>
+                <p> a. Students who have enrolled in any course leading to a degree program before enrollment in
+                    QCU.<br>
+                    b. Students who graduated from any I or 2 years Technical Vocational Courses.<br>
+                    c. 2nd Degree Course Taker (already a graduate of a Bachelor's Degree is not eligible to
+                    CHED-UniFAST)</p>
+            </div>
+        </div>
+        <div class="border border-black rounded-top-3 rounded-bottom-5" id="section">
+            <h2 class="text-white" id="subHeading">ADMISSIONS GUIDELINES: Qualifications</h2>
+            <p id="qcian-details" class="p-5 rounded-bottom-5">A student desiring to enroll in Quezon City University
+                has to comply with the
+                following
+                Admission
+                requirements: <br> <br>
 
-            1. Must pass the QCU Admission Test <br>
-            2. Must have at least a general weighted average of 80% or above for those who will take BSIT, BSENT,
-            and BSIE. For ESA, BSECE, and BECED, see their respective admission policy. <br>
-            3. Of good moral character<br>
-            4. Physically and Mentally fit to pursue a college education<br>
-            5. Must be a senior high school graduate, high school graduate (old curriculum), or ALS passer<br>
-            eligible for college<br>
-            6. Must pass the department interview</p>
-        <h2 class="text-white" id="subHeading">GENERAL ADMISSION POLICY</h2>
-        <p id="qcian-details" class="p-5">A student desiring to enroll in Quezon City University has to comply with the following
-            Admission
-            requirements: <br> <br>
+                1. Must pass the QCU Admission Test <br>
+                2. Must have at least a general weighted average of 80% or above for those who will take BSIT, BSENT,
+                and BSIE. For ESA, BSECE, and BECED, see their respective admission policy. <br>
+                3. Of good moral character<br>
+                4. Physically and Mentally fit to pursue a college education<br>
+                5. Must be a senior high school graduate, high school graduate (old curriculum), or ALS passer<br>
+                eligible for college<br>
+                6. Must pass the department interview</p>
+        </div>
+        <div class="border border-black rounded-top-3 rounded-bottom-5" id="section">
+            <h2 class="text-white" id="subHeading">GRADING SYSTEM</h2>
+            <p id="qcian-details" class="p-5 rounded-bottom-5">A student desiring to enroll in Quezon City University
+                has to comply with the
+                following
+                Admission
+                requirements: <br> <br>
 
-            1. Must pass the QCU Admission Test <br>
-            2. Must have at least a general weighted average of 80% or above for those who will take BSIT, BSENT,
-            and BSIE. For ESA, BSECE, and BECED, see their respective admission policy. <br>
-            3. Of good moral character<br>
-            4. Physically and Mentally fit to pursue a college education<br>
-            5. Must be a senior high school graduate, high school graduate (old curriculum), or ALS passer<br>
-            eligible for college<br>
-            6. Must pass the department interview</p>
+                1. Must pass the QCU Admission Test <br>
+                2. Must have at least a general weighted average of 80% or above for those who will take BSIT, BSENT,
+                and BSIE. For ESA, BSECE, and BECED, see their respective admission policy. <br>
+                3. Of good moral character<br>
+                4. Physically and Mentally fit to pursue a college education<br>
+                5. Must be a senior high school graduate, high school graduate (old curriculum), or ALS passer<br>
+                eligible for college<br>
+                6. Must pass the department interview</p>
+        </div>
     </div>
     <?php include('../html/footer.html') ?>
     <script src="../init.js"></script>
