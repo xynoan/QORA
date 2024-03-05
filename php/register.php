ac23c209ea03d575fee7b9ec2c042ded99ee281e
Nathaniel Carlo M. Morva <morva.nathanielcarlo.macapagal@gmail.com>
2024-03-05 08:13:51 +0800

    improve design

---
+++ php/register.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/init.css">
+    <link rel="stylesheet" href="../css/register.css">
+    <link rel="shortcut icon"
+        href="https://i0.wp.com/qcu.edu.ph/wp-content/uploads/2021/10/cropped-logo.jpg?fit=1500%2C1500&ssl=1"
+        type="image/x-icon">
+    <title>Quezon City University - Register</title>
+</head>
+
+<body>
+    <?php include('../html/header.html'); ?>
+    <div class="container">
+        <h1>Basic Information</h1>
+    </div>
+    <?php include('../html/footer.html'); ?>
+    <script src="../init.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/877d2cecdc.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
