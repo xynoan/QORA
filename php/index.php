4747834330ea5b6575f7ec6361bdde7d7b8ae431
xynoan <ncmorva@gmail.com>
2024-03-23 23:20:46 +0800

    initial homepage

--- php/index.php
+++ php/index.php
@@ -5,37 +5,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include('../html/link.html'); ?>
-    <link rel="stylesheet" href="../css/style.css">
+    <style>
+        h1 {
+            width: 60%;
+        }
+
+        section:first-child {
+            background-color: var(--secondary-color);
+        }
+
+        section:first-child>div,
+        section:nth-child(2)>div {
+            height: 100vh;
+        }
+
+        .border-primary {
+            border-color: #A08048 !important;
+        }
+    </style>
     <title>Quezon City University</title>
 </head>
 
 <body>
     <?php include('../html/header.html'); ?>
-    <div class="container" id="main-container">
-        <p class="fw-bold text-start py-5 dancing-script-header-description">Mula sa Tahanan ng Husay at Talino, para
-            sa Bayan...</p>
-        <div class="d-flex" id="content">
-            <div class="row row-cols-2 row-gap-3" id="grid">
-                <img src="../images/Admissions-Office.png" alt="">
-                <img src="../images/Image-1.png" alt="">
-                <img src="../images/Image-2.png" alt="">
-                <img src="../images/Image-3.png" alt="">
+    <main>
+        <section>
+            <div class="container d-flex align-items-end justify-content-end">
+                <h1 class="fw-bold text-center text-white text-uppercase p-4">30 years of empowering education from #1 city to global society</h1>
             </div>
-            <div class="d-flex flex-column justify-content-between" id="beside_grid">
-                <h1 class="text-white text-uppercase text-center">30 Years of Empowering Education from #1 City to
-                    Global
-                    Society</h1>
-                <div>
-                    <a class="text-white text-decoration-none d-inline-block fw-bold mb-2" href="./signin.php"
-                        id="signin">Sign
-                        in</a>
-                    <a class="text-white text-decoration-none d-inline-block fw-bold mt-2" href="./qcian.php"
-                        id="QCian">Be a
-                        QCian</a>
-                </div>
+        </section>
+        <section>
+            <div class="container">
+                <h2 class="fw-bold p-4 fs-2" style="color: var(--brown);">Hello, Future QCian!</h2>
             </div>
-        </div>
-    </div>
+        </section>
+    </main>
     <?php include('../html/footer.html'); ?>
     <?php include('../html/script.html'); ?>
 </body>
